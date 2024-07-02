a4cd38324ff4ee603933692daa00247ae6e0a464
Jadon Hansen <jadonhansen@gmail.com>
2024-07-02 21:01:47 +0200

    wip server

--- server/src/methods.ts
+++ server/src/methods.ts
@@ -1,10 +1,14 @@
 import { Error } from "./interfaces";
 import { getDataFromFile } from "./queries";
 
+const followersTxt = "../../db/followers.txt";
+const followingTxt = "../../db/following.txt";
+const postLikesTxt = "../../db/postLikes.txt";
+
 // find followers who do not like any of your posts
 export async function findGhostFollowers(): Promise<string[] | Error> {
-	const followers: string[] | Error = await getDataFromFile("../../followers");
-	const postLikes = await getDataFromFile("../../postLikes");
+	const followers: string[] | Error = await getDataFromFile(followersTxt);
+	const postLikes = await getDataFromFile(postLikesTxt);
 
 	if (!Array.isArray(followers)) return followers;
 	if (!Array.isArray(postLikes)) return postLikes;
@@ -15,8 +19,8 @@ export async function findGhostFollowers(): Promise<string[] | Error> {
 
 // find users who like your posts but do not follow you
 export async function findFans(): Promise<string[] | Error> {
-	const followers = await getDataFromFile("../../followers");
-	const postLikes = await getDataFromFile("../../postLikes");
+	const followers = await getDataFromFile(followersTxt);
+	const postLikes = await getDataFromFile(postLikesTxt);
 
 	if (!Array.isArray(followers)) return followers;
 	if (!Array.isArray(postLikes)) return postLikes;
@@ -27,8 +31,8 @@ export async function findFans(): Promise<string[] | Error> {
 
 // find users who do not follow you back
 export async function findUnfollowers(): Promise<string[] | Error> {
-	const followers = await getDataFromFile("../../followers");
-	const following = await getDataFromFile("../../following");
+	const followers = await getDataFromFile(followersTxt);
+	const following = await getDataFromFile(followingTxt);
 
 	if (!Array.isArray(followers)) return followers;
 	if (!Array.isArray(following)) return following;
@@ -39,8 +43,8 @@ export async function findUnfollowers(): Promise<string[] | Error> {
 
 // order your followers from the most interactive to the least interactive
 export async function orderedFollowers(): Promise<string[] | Error> {
-	const followers = await getDataFromFile("../../followers");
-	const postLikes = await getDataFromFile("../../postLikes");
+	const followers = await getDataFromFile(followersTxt);
+	const postLikes = await getDataFromFile(postLikesTxt);
 
 	if (!Array.isArray(followers)) return followers;
 	if (!Array.isArray(postLikes)) return postLikes;
